76fe1e7ee7ea7efb6bd7ef20859c9ed5b1264047
Morten <mtnielsen89@gmail.com>
2021-01-19 16:35:27 +0100

    Cleaned switch statement using a utility function for updating objects

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes"
+import {updateObject} from "../utility";
 
 const initialState = {
     counter: 0,
@@ -7,29 +8,17 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case (actionTypes.INCREMENT):
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
+            return updateObject(state, {counter: state.counter + action.value})
             
         case (actionTypes.DECREMENT):
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
+            return updateObject(state, {counter: state.counter - action.value})
         
         case (actionTypes.ADD):
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
+            return updateObject(state, {counter: state.counter + action.value})
         
         case (actionTypes.SUBTRACT):
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
-
+            return updateObject(state, {counter: state.counter - action.value})
+ 
         default:
             return state;
     }    
