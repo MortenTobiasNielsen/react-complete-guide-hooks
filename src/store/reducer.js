7a66dcfa2aadd0e7f95928fe7318d0bd781d29e6
Morten <mtnielsen89@gmail.com>
2021-01-19 08:56:34 +0100

    Added logic to store and delete results

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,23 +1,52 @@
 const initialState = {
     counter: 0,
+    results: [],
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case ("INCREMENT"):
-            return {counter: state.counter + action.value}
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
             
         case ("DECREMENT"):
-            return {counter: state.counter - action.value}
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
         
         case ("ADD"):
-            return {counter: state.counter + action.value}
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
         
         case ("SUBTRACT"):
-            return {counter: state.counter - action.value}
-    }
-    
-    return state;
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+
+        case ("STORE_RESULT"):
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+
+        case ("DELETE_RESULT"):
+            const updatedArray = state.results.filter(result => result.id !== action.ResultId);
+
+            return { 
+                ...state,
+                results: updatedArray,
+            }
+
+        default:
+            console.log("Reducer Action type not handled: " + action.type)
+            return state;
+    }    
 }
 
 export default reducer;
