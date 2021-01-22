83224c2c1cbb61b54906557219cc0336a4aa2142
Morten <mtnielsen89@gmail.com>
2021-01-22 14:59:34 +0100

    Replaced useState with useReducers

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, {useReducer, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
@@ -6,17 +6,49 @@ import Search from './Search';
 import ErrorModal from "../UI/ErrorModal";
 import {updateObject} from "../../shared/utility";
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case ("SET"):
+      return action.ingredients;
+    case ("ADD"):
+      console.log(action);
+      return [...currentIngredients, action.ingredient];
+    case ("DELETE"):
+      return currentIngredients.filter(ingredient => ingredient.id !== action.id)
+    default:
+      throw new Error("ingredientReducer action not handled");
+  };
+};
+
+const httpReducer = (httpState, action) => {
+  switch (action.type) {
+    case ("SEND"):
+      return {...httpState, isLoading: true, error: null};
+
+    case ("RESPONSE"):
+      return {...httpState, isLoading: false};
+
+    case ("ERROR"):
+      return {...httpState, isLoading: false, error: action.errorMessage};
+
+    case ("CLEAR"):
+      return {...httpState, error: null};
+
+    default:
+      throw new Error("httpReducer action not handled");
+  }
+};
+
 const Ingredients = () => {
-  const [ingredients, setIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const [ingredients, dispatchIngredients] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {isLoading: false, error: null});
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setIngredients(filteredIngredients);
+    dispatchIngredients({type: "SET", ingredients: filteredIngredients});
   }, []);
 
   const addIngredientHandler = ingredient => {
-    setIsLoading(true);
+    dispatchHttp({type: "SEND"});
 
     fetch("https://react-hooks-project-95b9d-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json", {
       method: "POST",
@@ -27,47 +59,43 @@ const Ingredients = () => {
       return response.json();
     })
     .then(responseData => {
-      console.log(responseData);
-      setIngredients(prevIngredients => [
-        ...prevIngredients, updateObject(ingredient, {id: responseData.name})
-      ]);
-      setIsLoading(false);
+      dispatchHttp({type: "RESPONSE"});
+      dispatchIngredients({
+        type: "ADD", 
+        ingredient: updateObject(ingredient, {id: responseData.name}),
+      });
     })
     .catch(error => {
-      setError(error.message);
-      setIsLoading(false);
+      dispatchHttp({type: "ERROR", errorMessage: error.message});
     });
   };
 
   const removeIngredientHandler = id => {
-    setIsLoading(true);
+    dispatchHttp({type: "SEND"});
     
     fetch(`https://react-hooks-project-95b9d-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${id}.json`, {
       method: "DELETE",
     })
     .then(response => {
-      setIngredients(prevIngredients => 
-        prevIngredients.filter(ingredient => ingredient.id !== id)
-      );
-      setIsLoading(false);
+      dispatchHttp({type: "RESPONSE"});
+      dispatchIngredients({type: "DELETE", id: id});
     })
     .catch(error => {
-      setError(error.message);
-      setIsLoading(false);
+      dispatchHttp({type: "ERROR", errorMessage: error.message});
     });
   };
 
   const clearError = () => {
-    setError(null);
+    dispatchHttp({type: "CLEAR"});
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
       
       <IngredientForm 
         onAddIngredient={addIngredientHandler}
-        loading={isLoading}
+        loading={httpState.isLoading}
       />
 
       <section>
