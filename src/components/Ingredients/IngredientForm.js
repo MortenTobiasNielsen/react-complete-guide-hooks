58abc6e8f023fc185d159ef304555b9b6c7bfcc5
Morten <mtnielsen89@gmail.com>
2021-01-22 07:53:27 +0100

    Added two way binding with the useState hook

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import {updateObject} from "../../shared/utility";
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const inputState = useState({
+    title: "",
+    amount: "",
+  });
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +21,27 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input 
+              type="text" 
+              id="title" 
+              value={inputState[0].title} 
+              onChange={event => {
+                const newTitle = event.target.value;
+                inputState[1](prevState => (updateObject(prevState, {title: newTitle})
+                ));
+              }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input 
+              type="number" 
+              id="amount" 
+              value={inputState[0].amount}
+              onChange={event => {
+                const newAmount = event.target.value;
+                inputState[1](prevState => (updateObject(prevState, {amount: newAmount})
+                ));
+              }}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
