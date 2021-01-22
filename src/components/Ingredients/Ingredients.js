b0c3cc88fbf9b27d0d638361a2af3c3b7cbd394a
Morten <mtnielsen89@gmail.com>
2021-01-22 12:56:30 +0100

    Added search capability - only exact match

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
@@ -7,22 +7,9 @@ import {updateObject} from "../../shared/utility";
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
-  
-  useEffect(() => {
-    fetch("https://react-hooks-project-95b9d-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json")
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        };
 
-        setIngredients(loadedIngredients);
-      });
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setIngredients(filteredIngredients);
   }, []);
 
   const addIngredientHandler = ingredient => {
@@ -53,7 +40,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         <IngredientList ingredients={ingredients} onRemoveItem={(id) => removeIngredientHandler(id)}/>
       </section>
     </div>
